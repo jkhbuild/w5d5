576619d253cf17d85ec37234185b1d1e5c66e56c
jkhbuild <jkh.build@gmail.com>
2022-10-14 14:50:37 -0400

    phase 1 completed

--- Movie Buff/movie_buff/02_queries.rb
+++ Movie Buff/movie_buff/02_queries.rb
@@ -1,11 +1,17 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
+
+  Movie.select(:id, :title, :score, :yr).where("yr BETWEEN 1980 AND 1989 AND score BETWEEN 3 AND 5")
+
+
   
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
+
+  Movie.select(:yr).group(:yr).where("score < 9")
   
 end
 
