044f262e70bfbd7f722603cbc98333bc37ba54d9
jkhbuild <jkh.build@gmail.com>
2022-10-14 15:42:40 -0400

    up to vanity projects

--- Movie Buff/movie_buff/02_queries.rb
+++ Movie Buff/movie_buff/02_queries.rb
@@ -11,13 +11,17 @@ end
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
 
-  Movie.select(:yr).group(:yr).where("score < 9")
+  Movie.select(:yr).group(:yr).having("MAX(score) < 8").pluck(:yr)
   
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  
+  Actor.joins(:movies).where('title in (?)', title).select(:id, :name).order("castings.ord ASC")
+
+
   
 end
 
